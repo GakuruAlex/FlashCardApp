3d7f85f806c7effe4b4c9f2905f5bbc49dae13ac
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-01 15:33:15 +0300

    UI

--- main.py
+++ main.py
@@ -1,27 +1,42 @@
 from tkinter import Tk, Button, Label, PhotoImage, Canvas
-
+iiiifrom flash_card import FlashCard
 class Main:
     BACKGROUND_COLOR = "#B1DDC6"
-    CANVAS_WIDTH = 600
-    CANVAS_HEIGHT = 426
+    
+    WINDOW_WIDTH = 900
+    WINDOW_HEIGHT = 626
+    CANVAS_WIDTH = 800
+    CANVAS_HEIGHT = 526
+    FONT = ("Rubik", 20)
     def ui(self):
         # main window
         window = Tk()
         window.title("Flash Card App")
-        window.config(background=self.BACKGROUND_COLOR, height=526, width=700, padx=50, pady= 50)
+        front_text = f"FRENCH"
+        back_text = f"ENGLISH"
+        window.config(background=self.BACKGROUND_COLOR, height=self.WINDOW_HEIGHT,width=self.WINDOW_WIDTH , padx=50, pady= 20)
+        #timer
+        timer = Label(window, text="3:00",)
+        timer.grid(row=0, column=1,)
         #canvas for the cards
-        canvas = Canvas(window, width=self.CANVAS_WIDTH, height = self.CANVAS_HEIGHT)
-        canvas .grid(row=0, column=0, columnspan=2, pady=20)
+        canvas = Canvas(window, width=self.CANVAS_WIDTH, height = self.CANVAS_HEIGHT, bg=self.BACKGROUND_COLOR)
+        canvas .grid(row=1, column=0, columnspan=2, )
+        #countdown
+        flashard = FlashCard()
+        #card
+        front_image = PhotoImage(file="./images/card_front.png")
+        canvas.create_image(400, 280, image=front_image)
+        canvas.create_text(400, 150, text=f"{front_text}", font=self.FONT)
         #buttons
         #right button and image
         right_image = PhotoImage(file="./images/right.png")
-        right = Button(window, image=right_image )
-        right.grid(row=1, column=0)
+        right = Button(window, image=right_image, highlightthickness=0 )
+        right.grid(row=2, column=0)
         #wrong button and image
         wrong_image = PhotoImage(file="./images/wrong.png")
-        wrong = Button(window, image=wrong_image)
-        wrong.grid(row=1, column=1)
-
+        wrong = Button(window, image=wrong_image, highlightthickness= 0)
+        wrong.grid(row=2, column=1)
+        
         window.mainloop()
     def main(self):
         self.ui()
