ccdaf6a91cd31b9edd0e998bae3dfa993b4c96bd
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-02 16:38:04 +0300

    clear the content and restart time if begin is pressed again

--- flash_card.py
+++ flash_card.py
@@ -13,19 +13,24 @@ class FlashCard:
         self.back_image = back_image
         self.data_dict = {}
         self.flash_cards()
-        
+        self.countdown = None
     def flash_cards(self):
         data = read_csv("./data/french_words.csv")
         self.data_dict = {row.French:row.English for index,row in data.iterrows()}
-    def time_countdown(self, new_time = 10):
+    def start_countdown(self):
+        if self.countdown is not None:
+            self.window.after_cancel(self.countdown)
+        self.time_countdown(10)
+    def time_countdown(self, new_time):
         display_time = gmtime(new_time)
         time = strftime("%S", display_time)
         self.timer.config(text = f"{time}")
         self.current_word = ""
         if new_time > 0:
             if new_time == 10:
+                self.clear_content()
                 self.display_front_card()
-            self.window.after(1000,  self.time_countdown,new_time - 1)
+            self.countdown =self.window.after(1000,  self.time_countdown,new_time - 1)
         elif new_time == 0:
             self.display_back_card()
     def display_front_card(self):
@@ -40,5 +45,8 @@ class FlashCard:
         self.english_word = self.data_dict[self.french_word]
         self.canvas.create_text(400, 150, text=f"ENGLISH", font=self.TITLE)
         self.canvas.create_text(400, 253, text=f"{self.english_word}", font=self.TEXT)
-        
+
+    def clear_content(self):
+        self.canvas.delete("all")
+
 
