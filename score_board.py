b8cfe0251b463ad46fb4bbae41281b43af20bd10
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-04 21:59:45 +0300

    get users name, get previous scores for existing user , save score against user name

--- score_board.py
+++ score_board.py
@@ -1,18 +1,29 @@
-from json import load, dump
-from tkinter import simpledialog
+from json import load, dump, JSONDecodeError
+from tkinter import simpledialog, messagebox
 class ScoreBoard:
     def __init__(self, window):
         self.window = window
         self.name = self.get_name()
-        self.score = self.get_score()
-    def get_score(self):
+        self.scores = []
+        self.score = 0
+    def get_scores(self):
         try:
             with open("./scoreboard.json", "r") as scoreboard:
-                data = load(scoreboard)
-                return data[self.name]
-        except FileNotFoundError:
-            return 0
+                self.data = load(scoreboard)
+                return self.data[self.name]
+        except FileNotFoundError as missing_file:
+            messagebox.showerror(title="File not Found!", message=f"{missing_file} not found")
     def get_name(self):
         name = simpledialog.askstring(title="Name",prompt="Enter your Name", parent=self.window)
-        return name
-    
+        if name in self.data:
+            answer = messagebox.askyesno(title="Name Exists", message="Name already exists do you want to change it ?")
+            if answer == "yes":
+                self.get_name()
+            return name
+    def save_name_and_score(self):
+        self.scores.append(self.score)
+        try:
+            with open("scoreboard.json", "w") as scoreboard:
+                dump({self.name: self.scores}, scoreboard, indent=4)
+        except JSONDecodeError as e:
+            messagebox.ERROR(e)
