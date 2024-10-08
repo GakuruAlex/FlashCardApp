edc124d64890fab35ee330a60f47aa214d7f2a52
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-08 12:51:35 +0300

    refactoring to use inheritance and abstraction

--- main.py
+++ main.py
@@ -11,45 +11,20 @@ class Main:
         # Store image references as instance variables
         self.front_image = None
         self.back_image = None
-        self.right_image = None
-        self.wrong_image = None
+        self.canvas = None
+        self.window = None
     def ui(self):
         # main window
-        window = Tk()
-       
-        
+        self.window = Tk()
 
-        window.title("Flash Card App")
-        window.config(background=self.BACKGROUND_COLOR, height=self.WINDOW_HEIGHT,width=self.WINDOW_WIDTH , padx=50, pady= 20)
-        #timer
-        timer = Label(window, text="00",)
-        timer.grid(row=0, column=1,)
+        self.window.title("Flash Card App")
+        self.window.config(background=self.BACKGROUND_COLOR, height=self.WINDOW_HEIGHT,width=self.WINDOW_WIDTH , padx=50, pady= 20)
         #canvas for the cards
-        canvas = Canvas(window, width=self.CANVAS_WIDTH, height = self.CANVAS_HEIGHT, bg=self.BACKGROUND_COLOR)
+        canvas = Canvas(self.window, width=self.CANVAS_WIDTH, height = self.CANVAS_HEIGHT, bg=self.BACKGROUND_COLOR)
         canvas .grid(row=1, column=0, columnspan=2, )
-        #background front 
-        self.front_image = PhotoImage(file="./images/card_front.png")
-        self.back_image = PhotoImage(file="./images/card_back.png")
-       
-        #countdown
-        flashcard = FlashCard(window=window, timer=timer, canvas=canvas,back_image=self.back_image, front_image=self.front_image)
-        begin=Button(window, text="Begin", command=flashcard.run_all_cards)
-        begin.grid(row = 0, column=0)
-        
-        #buttons
-        #right button and image
-        scorecard = flashcard.get_scorecard()
-        self.right_image = PhotoImage(file="./images/right.png")
-        right = Button(window, image=self.right_image, highlightthickness=0 , command=scorecard.increase_score)
-        right.grid(row=2, column=0)
-        #wrong button and image
-        self.wrong_image = PhotoImage(file="./images/wrong.png")
-        wrong = Button(window, image=self.wrong_image, highlightthickness= 0, command=scorecard.got_wrong)
-        wrong.grid(row=2, column=1)
-        window.mainloop()
+
+        self.window.mainloop()
     def main(self):
         self.ui()
 
-if __name__ == "__main__":
-    main = Main()
-    main.main()
+
