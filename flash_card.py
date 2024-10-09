5eee71f129c4019ddbc2d8f962c5655a67b9ea85
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-09 13:42:21 +0000

    display front car with french word

--- flash_card.py
+++ flash_card.py
@@ -1,22 +1,25 @@
-from ui import UI
 from pandas import read_csv, DataFrame
-from tkinter import PhotoImage, Text
+from tkinter import PhotoImage
+from random import choice
 
-class FlashCard(UI):
-    TITLE_FONT = ("Serif", 20, "italic")
+class FlashCard():
+    TITLE_FONT = ("Serif", 40, "bold")
     CONTENT_FONT = ("Serif", 24, "normal")
-    def __init__(self):
-        super().__init__()
+    def __init__(self, window, canvas):
+        self.window = window
+        self.canvas = canvas
         self.cards = {}
         self.english = None
         self.french = None
         self.get_data()
+        self.display_front_card()
     def get_data(self):
-        data = DataFrame(read_csv("./data/french_words.csv"))
+        data = DataFrame(read_csv("/home/aleyg/projects/Python/100DaysOfCode/DayThirty/FlashCardApp/data/french_words.csv"))
         self.cards = {row.French : row.English for index, row in data.iterrows()}
-        print(f"Data {self.cards}")
     def display_front_card(self):
-         self.front_image = PhotoImage(file="./images/card_front.png")
-         self.canvas.create_image(400, 263, image=self.front_image, parent=self.canvas)
-         self.front = Text(parent=self.canvas,str=f"French", font=self.TITLE_FONT)
-         self.front.grid(row=1, column=2)
+        self.french = choice(list(self.cards.keys()))
+        self.front_image = PhotoImage(file="/home/aleyg/projects/Python/100DaysOfCode/DayThirty/FlashCardApp/images/card_front.png", master=self.canvas)
+        self.canvas.create_image(400, 280, image=self.front_image)
+        self.canvas.create_text(400, 100, text=f"FRENCH", font=self.TITLE_FONT)
+        self.canvas.create_text(400, 200, text=f"{self.french}", font=self.CONTENT_FONT)
+
