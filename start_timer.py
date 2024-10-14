35e5aa8f660e8f3b9c78e3ce443a01fbc3b50060
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-14 19:14:52 +0300

    add start button and checking for cards

--- start_timer.py
+++ start_timer.py
@@ -1,33 +1,39 @@
 from time import strftime,gmtime
-from tkinter import Label, Canvas
+from tkinter import  Canvas, Button
 from flash_card import FlashCard
 class Timer():
     def __init__(self, window, canvas):
         self.BACKGROUND = "white"
-        self.seconds = 10
-        self.top_canvas = None
+        self.seconds = 5
         self.window = window
         self.canvas = canvas
-        self.TIMER_FONT = ("Serif", 20, "bold")
+        self.top_canvas = None
+        self.timer_id = None
+        self.TIMER_FONT = ("Serif", 10, "bold")
         self.flashcard = FlashCard(self.window, self.canvas)
+        self.timer_ui()
 
     def timer_ui(self,):
-        self.top_canvas = Canvas(self.window, width= 200, height= 100,bg=self.BACKGROUND)
-        self.top_canvas.grid(row= 0, column= 2, pady= 5)
-        self.text_id =self.top_canvas.create_text(100,50, text="00", font= self.TIMER_FONT)
+        button = Button(self.window, text="start", command=self.start_timer)
+        button.grid(row=0, column=0,)
+        self.top_canvas = Canvas(self.window, width= 50, height= 20,bg=self.BACKGROUND)
+        self.top_canvas.grid(row= 0, column= 2,)
+        self.text_id =self.top_canvas.create_text(25,10, text="00", font= self.TIMER_FONT)
     def start_timer(self):
-        self.flashcard.display_front_card()
-        self.run_timer(self.seconds)
-    def run_timer(self, seconds):
+        if self.flashcard.check_for_cards():
+            self.flashcard.display_front_card()
+            self.run_timer(self.seconds)
+    def run_timer(self, seconds: int):
         time = strftime("%S", gmtime(seconds))
         self.top_canvas.itemconfig(self.text_id,text=f"{time}")
         if seconds > 0:
-            self.timer_id = self.window.after(1000, self.run_timer, seconds - 1)
-        elif seconds == 0:
-            self.flashcard.display_back_card()
-            self.window.after(4000, self.check_for_cards)
-    def check_for_cards(self):
-        if len(self.flashcard.cards) > self.flashcard.counter:
-                self.start_timer()
+                self.timer_id = self.window.after(1000, self.run_timer, seconds - 1)
+        else:
+                self.flashcard.display_back_card()
+                self.flashcard.counter += 1
+                if self.flashcard.check_for_cards():
+                    self.window.after(4000,self.start_timer)
     def cancel_current(self):
         self.window.after_cancel(self.timer_id)
+        self.start_timer()
+
