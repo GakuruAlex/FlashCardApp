9bfef8ddc67acf8cabf9d1228b9189695957bdb6
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-14 19:12:43 +0300

    initialize to learn

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flash_card import FlashCard
 from ui import UI
 from start_timer import Timer
+from to_learn import ToLearn
 def main():
     ui = UI()
     window = ui.get_window_and_canvas()["window"]
     canvas = ui.get_window_and_canvas()["canvas"]
     flashcard = FlashCard(window=window, canvas=canvas)
     timer = Timer(window=window, canvas=canvas)
-    timer.timer_ui()
-    timer.start_timer()
+    to_learn = ToLearn(window=window, canvas=canvas)
     ui.start()
 if __name__ == "__main__":
     main()
