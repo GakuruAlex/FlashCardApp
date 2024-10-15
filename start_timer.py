179b37bfb376404e5a63d414525656beca52ad1e
alexgicimo <gakurualexgicimo@gmail.com>
2024-10-15 10:31:47 +0000

    check if its time to save the words that are unkown to the user

--- start_timer.py
+++ start_timer.py
@@ -2,16 +2,18 @@ from time import strftime,gmtime
 from tkinter import  Canvas, Button
 from flash_card import FlashCard
 class Timer():
-    def __init__(self, window, canvas):
+    def __init__(self, window, canvas, main_class):
         self.BACKGROUND = "white"
         self.seconds = 5
         self.window = window
         self.canvas = canvas
+        self.words_combo = {}
+        self.main_class = main_class
         self.top_canvas = None
         self.timer_id = None
         self.TIMER_FONT = ("Serif", 10, "bold")
-        self.flashcard = FlashCard(self.window, self.canvas)
-        self.timer_ui()
+        self.flashcard = FlashCard(self.window, self.canvas, main_class= self.main_class)
+       
 
     def timer_ui(self,):
         button = Button(self.window, text="start", command=self.start_timer)
@@ -21,6 +23,7 @@ class Timer():
         self.text_id =self.top_canvas.create_text(25,10, text="00", font= self.TIMER_FONT)
     def start_timer(self):
         if self.flashcard.check_for_cards():
+            self.words_combo =self.flashcard.get_card()
             self.flashcard.display_front_card()
             self.run_timer(self.seconds)
     def run_timer(self, seconds: int):
@@ -30,10 +33,12 @@ class Timer():
                 self.timer_id = self.window.after(1000, self.run_timer, seconds - 1)
         else:
                 self.flashcard.display_back_card()
-                self.flashcard.counter += 1
                 if self.flashcard.check_for_cards():
                     self.window.after(4000,self.start_timer)
     def cancel_current(self):
         self.window.after_cancel(self.timer_id)
+        self.canvas.delete("all")
         self.start_timer()
+    def check_if_to_save(self):
+        return self.flashcard.counter == self.flashcard.length
 
